ad1dcd2b2202ce5adf2143bccd7ebcbcdc0f0ac1
Yuri Ramos <yurigabriel21@yahoo.com>
2024-12-11 03:51:23 -0300

    feat: criacao pagina inicial login responsivo

--- front-sistema-ponto-ilumeo/src/@global/paginas/publicas/Login.pagina.tsx
+++ front-sistema-ponto-ilumeo/src/@global/paginas/publicas/Login.pagina.tsx
@@ -1,29 +1,53 @@
 import { Box, Button, Container, Flex, Image, Input, Text } from "@chakra-ui/react"
 import { coresUi } from "../../../themes/coresUi"
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { rotasApp } from "../../../rotas/rotasApp";
+
+
+type InputLogin = {
+  codigo_funcionario: string
+}
+
 
 export const LoginPagina = () => {
+
+  const navigate = useNavigate()
+
+    const { register, watch, getValues } = useForm<InputLogin>();
+
+    console.log(watch("codigo_funcionario"))
+
   return (
-        <Container w={"100vw"} h={"100vh"} bg={coresUi.neutra} display={"flex"}>
-          <Box display={"flex"} justifyContent={'center'} alignItems={'center'} w={"50%"} h={"100vh"} background={coresUi.neutra}>
+        <Container w={"100vw"} h={"100vh"} bg={coresUi.neutra} display={"flex"} justifyContent={{base:"center", lg:"none"}}>
+
+          <Box display={"flex"} justifyContent={'center'} alignItems={'center'} w={{base:"100%", lg:"50%"}} h={"100vh"} background={coresUi.neutra}>
+
             <Flex direction={"column"}  w="90%" height={"200px"}>
+
               <Box>
               <Flex fontSize={"40px"} gap={"10px"} >
                 <Text fontWeight={"600"} color={coresUi.primaria}> Ponto </Text>
                 <Text fontWeight={"700"} color={coresUi.secundaria}> Ilumeo </Text>
               </Flex>
+
               <Flex direction="column" gap={"18px"}>
-              <Input placeholder="Código do funcionário:" />
-              <Button bg={coresUi.secundaria}>
-                <Text textTransform={"uppercase"} fontWeight={"600"} fontSize={"18px"} color={coresUi.neutra}>Confirmar</Text>
-              </Button>
+                <Input {...register("codigo_funcionario")}  placeholder="Código do funcionário:" focusRingColor={coresUi.secundaria} padding={"15px"} color={coresUi.primaria} fontWeight={"700"}/>
+
+                <Button bg={coresUi.secundaria} onClick={()=> navigate(rotasApp.funcionarioPage(getValues("codigo_funcionario")))}>
+                  
+                  <Text textTransform={"uppercase"} fontWeight={"600"} fontSize={"18px"} color={coresUi.neutra}>Confirmar</Text>
+                </Button>
+
+
               </Flex>
+
               </Box>
-            
-              
+          
             </Flex>
           </Box>
 
-          <Flex justifyContent={'center'} alignItems={'center'} w={"50%"} h={"100vh"} background={coresUi.primaria}>
+          <Flex display={{base:"none", lg:"flex"}} justifyContent={'center'} alignItems={'center'} w={"50%"} h={"100vh"} background={coresUi.backgroundMain}>
             <Image src="/imagem-home.png" w="500px" h="590px" />
           </Flex>
         </Container>
