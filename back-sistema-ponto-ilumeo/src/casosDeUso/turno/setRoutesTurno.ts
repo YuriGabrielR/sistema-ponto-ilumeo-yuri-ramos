55168e2a4eb5a9d4810697cfbc8e68767d69d96e
Yuri Ramos <yurigabriel21@yahoo.com>
2024-12-12 15:07:11 -0300

    feat: implementa novas rotas relacionadas a entidade turno

--- back-sistema-ponto-ilumeo/src/casosDeUso/turno/setRoutesTurno.ts
+++ back-sistema-ponto-ilumeo/src/casosDeUso/turno/setRoutesTurno.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import { turnoCriarRouter } from "./criar/turnoCriar.router";
+import { turnoFinalizar } from "./finalizarTurno/turnoFinalizar.router";
 
 
 
 export function setRoutesTurno(baseUrlPath: string, expressApp: Application) {
     expressApp.use(baseUrlPath, turnoCriarRouter())
+    expressApp.use(baseUrlPath,turnoFinalizar())
 }
--- back-sistema-ponto-ilumeo/src/configurações/rotas/rotasApiTurno.ts
+++ back-sistema-ponto-ilumeo/src/configurações/rotas/rotasApiTurno.ts
@@ -1,4 +1,5 @@
 
 export const rotasApiTurno = {
     turnoCriar: '/turno',
+    turnoFinalizar:'turno/finalizar',
 }
