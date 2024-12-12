ab4d0e88e2195802e38ff83e14582e226a1a8f3c
Yuri Ramos <yurigabriel21@yahoo.com>
2024-12-12 08:19:59 -0300

    fix: deleção de arquivos desnecessários

--- back-sistema-ponto-ilumeo/src/@types/httpServer.ts
+++
@@ -1,8 +0,0 @@
-import { type Application, type NextFunction, type Request, type Response, type Router } from 'express'
-
-export type IResponse = Response
-
-export type IRequest = Request
-export type INextFunction = NextFunction
-export type IRouter = Router
-export type IExpressApp = Application
