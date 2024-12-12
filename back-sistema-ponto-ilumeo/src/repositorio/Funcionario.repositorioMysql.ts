d49d36703a9a78313f3c3a0983e691744cf8926a
Yuri Ramos <yurigabriel21@yahoo.com>
2024-12-12 08:22:32 -0300

    feat: adicao da camada repositorio ao projeto e implementacao do metodo buscarPorCodigo

--- back-sistema-ponto-ilumeo/src/repositorio/Funcionario.repositorioMysql.ts
+++ back-sistema-ponto-ilumeo/src/repositorio/Funcionario.repositorioMysql.ts
@@ -1,8 +1,9 @@
-import { prisma } from "@/configurações/databases/mysql/iniciarConexãoMysql";
-import { FuncionarioModelDTO, IFuncionarioModelAtributos } from "@/models/FuncionarioModel";
+import { prisma } from "../configurações/databases/mysql/iniciarConexãoMysql";
+import { FuncionarioModelDTO, IFuncionarioModelAtributos } from "../models/FuncionarioModel";
+import { TurnoModelDTO } from "../models/TurnoModel";
 
 export class FuncionarioRepositorioMysql {
-  async buscarPorCodigo({ codigoFuncionario }: IBuscarPorCodigo): Promise<FuncionarioModelDTO> {
+  async buscarPorCodigo({ codigoFuncionario }: IBuscarPorCodigo): Promise<FuncionarioModelDTO| null> {
     try {
       const result = await prisma.funcionario.findUnique({
         where: { codigo: codigoFuncionario },
@@ -11,8 +12,23 @@ export class FuncionarioRepositorioMysql {
         },
       });
 
-      
-      if (!result) return undefined;
+      if (!result) return null;
+
+      const turno = result.turnos ? result.turnos.map(turno => new TurnoModelDTO({
+        id: turno.id, 
+        funcionarioId: turno.funcionarioId as number,
+        inicioTurno: turno.inicioTurno|| undefined,
+        fimTurno: turno.fimTurno || undefined,
+      })) : [];
+
+      return new FuncionarioModelDTO({
+        id: result.id, 
+        codigo: result.codigo,
+        nome: result.nome,
+        criadoEm: result.criadoEm, 
+        atualizadoEm: result.atualizadoEm,
+        turnos: turno,
+      });
     } catch (erro) {
       console.log("PerfilRepositorioMysql buscarPorId");
       console.log(erro);
